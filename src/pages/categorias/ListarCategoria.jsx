307f9b5a5601b4a847f68081111e892d2ddcd624
arthuurgab <adarocha060@gmail.com>
2025-05-13 21:59:49 -0300

    fim de aula

--- src/pages/categorias/ListarCategoria.jsx
+++ src/pages/categorias/ListarCategoria.jsx
@@ -7,9 +7,41 @@ function ListarCategoria() {
       <Navbar />
       <TituloLista
         titulo="Categorias"
-        descrição="Gerencie aqui os livros..."
+        descricao="Gerencie aqui os livros..."
         rota="/cadastrar_categoria"
       />
+      <div className="overflow-x-auto">
+        <table className="min-w-full text-sm text-left border-t border-gray-300">
+          <thead className="text-gray-700 uppercase">
+            <tr>
+              <th className="py-2 px-4">#</th>
+              <th className="py-2 px-4">First</th>
+              <th className="py-2 px-4">Last</th>
+              <th className="py-2 px-4">Handle</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-800">
+            <tr className="border-t">
+              <td className="py-2 px-4">1</td>
+              <td className="py-2 px-4">Mark</td>
+              <td className="py-2 px-4">Otto</td>
+              <td className="py-2 px-4">@mdo</td>
+            </tr>
+            <tr className="border-t">
+              <td className="py-2 px-4">2</td>
+              <td className="py-2 px-4">Jacob</td>
+              <td className="py-2 px-4">Thornton</td>
+              <td className="py-2 px-4">@fat</td>
+            </tr>
+            <tr className="border-t">
+              <td className="py-2 px-4">3</td>
+              <td className="py-2 px-4">Larry the Bird</td>
+              <td className="py-2 px-4"></td>
+              <td className="py-2 px-4">@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/routes/Rotas.jsx
+++ src/routes/Rotas.jsx
@@ -2,16 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import App from "../App";
 import CadastrarCategoria from "../pages/categorias/CadastrarCategoria";
 import ListarCategoria from "../pages/categorias/ListarCategoria";
-import Detalhes from "../pages/detalhes/Detalhe";
 
 function Rotas() {
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/cadastrar_categoria" element={<CadastrarCategoria />} />
-      <Route path="/listar_categoria" element={<ListarCategoria />} />
-      <Route path="/detalhes" element={<Detalhes />} />
-    </Routes>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/cadastrar_categoria" element={<CadastrarCategoria />} />
+        <Route path="/listar_categoria" element={<ListarCategoria />} />
+      </Routes>
+    </div>
   );
 }
 
